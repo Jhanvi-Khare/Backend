c5e8e16e0b2d3b77bcf1609ab7b8713eee9f4eed
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-04-23 18:49:46 +0530

    mangodb

--- connection.js
+++ connection.js
@@ -1,5 +1,4 @@
 const mongoose = require( 'mongoose');
-
 const url = "mongodb+srv://jhanvikhare26:water1@cluster0.jfuhq8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 // asynchronous function
