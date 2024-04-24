242222056206028f4dfd836d90f86f655a8b9640
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-04-24 15:18:40 +0530

    user modules

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require( 'mongoose');
-const url = "mongodb+srv://jhanvikhare26:water1@cluster0.jfuhq8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const url = "mongodb+srv://jhanvikhare26:water1@cluster0.jfuhq8r.mongodb.net/mydatabase1234?retryWrites=true&w=majority&appName=Cluster0"
 
 // asynchronous function
 mongoose.connect(url)
@@ -10,4 +10,4 @@ mongoose.connect(url)
     console.log(err);
 });
 
-console.log('something else');
+module.exports = mongoose;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Model = require('../models/userModel');
 
 router.get('/add', (req, res) => {
-    res.send('Respond from user add')
-})
+    res.send('Respond from user add');
+});
 
 //getall
+router.get('/getall', (req, res) => {
+    res.send('Respond from user getall');
+});
+
 //getbyid
+router.get('/getbyid', (req, res) => {
+    res.send('Respond from user getbyid');
+});
+
 //delete
+router.get('/delete', (req, res) => {
+    res.send('Respond from user delete');
+});
+
 //update
+router.get('/update', (req, res) => {
+    res.send('Respond from user update');
+});
+
 
 module.exports = router;
