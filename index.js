a60a48b2a4f7f1039a66f1d2d1fc705f69b9f824
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-04-26 17:30:50 +0530

    login page

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 //import express
-const express = require('express')
-const userRouter = require('./routers/userRouter')
+const express = require('express');
+const userRouter = require('./routers/userRouter');
 
 //initialize express
-const app = express()
+const app = express();
 const port = 5000;
 
 //middleware
-app.use( '/user', userRouter )
+app.use( '/user', userRouter );
 
 // endpoint
 app.get('/', (req, res) => {
-    res.send('Response from express')
-})
+    res.send('Response from express');
+});
 
 // add
 app.get('/add', (req, res) => {
-    res.send('Response from add')
-})
+    res.send('Response from add');
+});
 
 //delete
 app.get('/del', (req, res) => {
-    res.send('Response from del')
-})
+    res.send('Response from del');
+});
 
 // update
 app.get('/update', (req, res) => {
-    res.send('Response from update')
-})
+    res.send('Response from update');
+});
 
 // start server
-app.listen(port, () => { console.log('server started'); })
+app.listen(port, () => { console.log('server started'); });
